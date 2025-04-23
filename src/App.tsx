fdac332deab63c8672acc514ec35350cd456e87e
William McGuire <will@w3dv.com>
2025-04-23 05:18:42 +0200

    Simplify App component and remove container div

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,22 @@ import { createRoot } from 'react-dom/client';
 import MetisCard from './components/MetisCard';
 import './styles/App.css';
 
-interface AppProps {
-  containerRef?: React.RefObject<HTMLDivElement>;
-}
+const App: React.FC = () => (
+  <MetisCard 
+    address="0x6Cb5365ADfC0fdFc2aD7C02151A04e9f0F9eBeCA"
+    threshold={5000}
+  />
+);
 
-const App: React.FC<AppProps> = ({ containerRef }) => {
-  return (
-    <div ref={containerRef} className="metis-widget-container">
-      <MetisCard 
-        address="0x6Cb5365ADfC0fdFc2aD7C02151A04e9f0F9eBeCA"
-        threshold={5000}
-      />
-    </div>
-  );
-};
-
-// Export for direct use
 export default App;
 
-// Export for embedding
 export function embedMetisWidget(container: HTMLElement) {
-  const root = document.createElement('div');
-  container.appendChild(root);
-  
-  const reactRoot = createRoot(root);
+  const reactRoot = createRoot(container);
   reactRoot.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
   
-  return () => {
-    reactRoot.unmount();
-    container.removeChild(root);
-  };
+  return () => reactRoot.unmount();
 }
